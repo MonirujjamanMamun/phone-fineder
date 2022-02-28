9531e1902051d8c89d23703230dcc11421adb366
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-02-28 23:43:33 +0600

    single data display complied

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const loadData = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data))
+
 }
 
 const displayPhone = (numbers) => {
@@ -20,10 +21,43 @@ const displayPhone = (numbers) => {
                 <h5>${number.brand}</h5>
             </div>
             <div class='text-center my-2'>
-            <button type="button" class="btn btn-success ">Details</button>
+            <button onclick="loadSingleData('${number.slug}')" type="button" class="btn btn-success ">Details</button>
             </div>
         </div>
         `;
         allPhone.appendChild(div);
     });
+}
+
+const loadSingleData = (number) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${number}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySingleData(data.data))
+}
+
+
+const displaySingleData = (number) => {
+        // console.log(number)
+        const singlePhone = document.getElementById("single-phone");
+        const div = document.createElement('div');
+        div.classList.add("card")
+        div.innerHTML = `
+        <img src="${number.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h1 class="card-title">Name : ${number.name}</h1>
+            <h2 class="card-title">Brand : ${number.brand}</h2>
+            <h3 class="card-title">Release Date : ${number.releaseDate||'release date not found'}</h3>
+            <p class="card-text">Storage : ${number.mainFeatures.storage}</p>
+            <p class="card-text">DisplaySize : ${number.mainFeatures.displaySize}</p>
+            <p class="card-text">Chip Set : ${number.mainFeatures.chipSet}</p>
+            <p class="card-text">Memory : ${number.mainFeatures.memory}</p>
+            <p class="card-text">Sensors : ${number.mainFeatures.sensors}</p>
+
+    
+            <p class="card-text">${number.others? `Bluetooth : ${number.others.Bluetooth}, ${number.others.GPS},${number.others.NFC}, ${number.others.Radio}, ${number.others.USB},${number.others.WLAN}` : 'Not found'}</p>
+           
+        </div>
+    `;
+    singlePhone.appendChild(div);
 }
