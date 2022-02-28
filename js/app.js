f0bad91be6bea35701d4671ec7a13bcfde59543e
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-03-01 01:29:23 +0600

    error handaling complited

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
 const loadData = () => {
     const inputFild = document.getElementById("input-fild");
     const inputValue = inputFild.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data))
+    inputFild.value = '';
 
+    if (inputValue === '') {
+        document.getElementById("empty-fild").style.display = 'block';
+    } else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhone(data.data.slice(0, 20)))
+        document.getElementById("empty-fild").style.display = 'none';
+    }
 }
 
 const displayPhone = (numbers) => {
     const allPhone = document.getElementById("all-phone");
-    numbers.forEach(number => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    allPhone.textContent = '';
+    document.getElementById("single-phone").textContent = '';
+
+    if (numbers.length === 0) {
+        document.getElementById("error").style.display = 'block';
+    } else {
+
+        numbers.forEach(number => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
         <div class="card">
             <img src="${number.image}" class="card-img-top" alt="">
             <div class="card-body text-center">
@@ -25,8 +38,10 @@ const displayPhone = (numbers) => {
             </div>
         </div>
         `;
-        allPhone.appendChild(div);
-    });
+            allPhone.appendChild(div);
+        });
+        document.getElementById("error").style.display = 'none';
+    }
 }
 
 const loadSingleData = (number) => {
@@ -40,24 +55,22 @@ const loadSingleData = (number) => {
 const displaySingleData = (number) => {
         // console.log(number)
         const singlePhone = document.getElementById("single-phone");
-        const div = document.createElement('div');
-        div.classList.add("card")
-        div.innerHTML = `
-        <img src="${number.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h1 class="card-title">Name : ${number.name}</h1>
-            <h2 class="card-title">Brand : ${number.brand}</h2>
-            <h3 class="card-title">Release Date : ${number.releaseDate||'release date not found'}</h3>
-            <p class="card-text">Storage : ${number.mainFeatures.storage}</p>
-            <p class="card-text">DisplaySize : ${number.mainFeatures.displaySize}</p>
-            <p class="card-text">Chip Set : ${number.mainFeatures.chipSet}</p>
-            <p class="card-text">Memory : ${number.mainFeatures.memory}</p>
-            <p class="card-text">Sensors : ${number.mainFeatures.sensors}</p>
+        singlePhone.innerHTML = `
+        <div class="card">
+        <img src="${number.image}" class="w-50 mx-auto mt-2" alt="">
+        <div class="card-body ms-5">
+            <h3><span class='fw-bold'>Name : </span>${number.name}</h3>
+            <h4><span class='fw-bold'>Brand : </span>${number.brand}</h4>
+            <h6><span class='fw-bold'>Release Date : </span>${number.releaseDate||'release date not found'}</h6>
+            <p><span class='fw-bold'>Storage : </span>${number.mainFeatures.storage}</p>
+            <p><span class='fw-bold'>DisplaySize : </span>${number.mainFeatures.displaySize}</p>
+            <p><span class='fw-bold'>Chip Set : </span>${number.mainFeatures.chipSet}</p>
+            <p><span class='fw-bold'>Memory : </span>${number.mainFeatures.memory}</p>
+            <p><span class='fw-bold'>Sensors : </span>${number.mainFeatures.sensors}</p>
 
-    
-            <p class="card-text">${number.others? `Bluetooth : ${number.others.Bluetooth}, ${number.others.GPS},${number.others.NFC}, ${number.others.Radio}, ${number.others.USB},${number.others.WLAN}` : 'Not found'}</p>
+            <p>${number.others? `<span class='fw-bold'>Bluetooth : </span>${number.others.Bluetooth}, ${number.others.GPS},${number.others.NFC}, ${number.others.Radio}, ${number.others.USB},${number.others.WLAN}` : 'Not found'}</p>
            
         </div>
+        </div>
     `;
-    singlePhone.appendChild(div);
 }
