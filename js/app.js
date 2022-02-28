4548efb2d2555555c2b115e867be7b8f805ffa0c
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-03-01 02:01:44 +0600

    validation added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const loadData = () => {
     const inputFild = document.getElementById("input-fild");
-    const inputValue = inputFild.value;
+    const inputValue = inputFild.value.toLowerCase();
     inputFild.value = '';
 
     if (inputValue === '') {
@@ -22,24 +22,24 @@ const displayPhone = (numbers) => {
     if (numbers.length === 0) {
         document.getElementById("error").style.display = 'block';
     } else {
-
         numbers.forEach(number => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-        <div class="card">
-            <img src="${number.image}" class="card-img-top" alt="">
-            <div class="card-body text-center">
-                <h3 class="card-title">${number.phone_name}</h3>
-                <h5>${number.brand}</h5>
-            </div>
-            <div class='text-center my-2'>
-            <button onclick="loadSingleData('${number.slug}')" type="button" class="btn btn-success ">Details</button>
-            </div>
-        </div>
-        `;
+                <div class="card">
+                    <img src="${number.image}" class="card-img-top" alt="">
+                    <div class="card-body text-center">
+                        <h3 class="card-title">${number.phone_name}</h3>
+                        <h5>${number.brand}</h5>
+                    </div>
+                    <div class='text-center my-2'>
+                    <button onclick="loadSingleData('${number.slug}')" type="button" class="btn btn-success ">Details</button>
+                    </div>
+                </div>
+            `;
             allPhone.appendChild(div);
         });
+
         document.getElementById("error").style.display = 'none';
     }
 }
@@ -51,26 +51,24 @@ const loadSingleData = (number) => {
         .then(data => displaySingleData(data.data))
 }
 
-
 const displaySingleData = (number) => {
         // console.log(number)
         const singlePhone = document.getElementById("single-phone");
         singlePhone.innerHTML = `
         <div class="card">
-        <img src="${number.image}" class="w-50 mx-auto mt-2" alt="">
-        <div class="card-body ms-5">
-            <h3><span class='fw-bold'>Name : </span>${number.name}</h3>
-            <h4><span class='fw-bold'>Brand : </span>${number.brand}</h4>
-            <h6><span class='fw-bold'>Release Date : </span>${number.releaseDate||'release date not found'}</h6>
-            <p><span class='fw-bold'>Storage : </span>${number.mainFeatures.storage}</p>
-            <p><span class='fw-bold'>DisplaySize : </span>${number.mainFeatures.displaySize}</p>
-            <p><span class='fw-bold'>Chip Set : </span>${number.mainFeatures.chipSet}</p>
-            <p><span class='fw-bold'>Memory : </span>${number.mainFeatures.memory}</p>
-            <p><span class='fw-bold'>Sensors : </span>${number.mainFeatures.sensors}</p>
+            <img src="${number.image}" class="w-50 mx-auto mt-2" alt="">
+            <div class="card-body ms-5">
+                <h3><span class='fw-bold'>Name : </span>${number.name}</h3>
+                <h4><span class='fw-bold'>Brand : </span>${number.brand}</h4>
+                <h6><span class='fw-bold'>Release Date : </span>${number.releaseDate||'release date not found'}</h6>
+                <p><span class='fw-bold'>Storage : </span>${number.mainFeatures.storage}</p>
+                <p><span class='fw-bold'>DisplaySize : </span>${number.mainFeatures.displaySize}</p>
+                <p><span class='fw-bold'>Chip Set : </span>${number.mainFeatures.chipSet}</p>
+                <p><span class='fw-bold'>Memory : </span>${number.mainFeatures.memory}</p>
+                <p><span class='fw-bold'>Sensors : </span>${number.mainFeatures.sensors}</p>
 
-            <p>${number.others? `<span class='fw-bold'>Bluetooth : </span>${number.others.Bluetooth}, ${number.others.GPS},${number.others.NFC}, ${number.others.Radio}, ${number.others.USB},${number.others.WLAN}` : 'Not found'}</p>
-           
-        </div>
-        </div>
-    `;
+                <p>${number.others? `<span class='fw-bold'>Bluetooth : </span>${number.others.Bluetooth}, ${number.others.GPS},${number.others.NFC}, ${number.others.Radio}, ${number.others.USB},${number.others.WLAN}` : 'Not found'}</p>
+            
+            </div>
+        </div>`;
 }
